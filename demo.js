5df25768670bc1c221c30694c6e2d3b7683979d5
axhar-ibraheem <azharibraheem482@gmail.com>
2023-01-11 12:31:45 +0530

    new files added

--- demo.js
+++ demo.js
@@ -1,1 +1,3 @@
-console.log("hello world");
+document.querySelector("#header-title").style.border = "2px solid black";
+document.querySelector(".title").style.color = "green";
+document.querySelector(".title").style.fontWeight = "bold";
