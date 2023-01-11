b10ba2316be66b27cf3f5aa4e2f7813fbfddb8f9
axhar-ibraheem <azharibraheem482@gmail.com>
2023-01-11 13:26:04 +0530

    demo.js modified

--- demo.js
+++ demo.js
@@ -1,3 +1,8 @@
 document.querySelector("#header-title").style.border = "2px solid black";
 document.querySelector(".title").style.color = "green";
 document.querySelector(".title").style.fontWeight = "bold";
+var items = document.getElementsByClassName("list-group-item");
+items[2].style.backgroundColor = "green";
+for (let i = 0; i < items.length; i++) {
+  items[i].style.fontWeight = "bold";
+}
