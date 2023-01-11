740f9ded6457db4bca025e6e6041b4f74a856ee2
axhar-ibraheem <azharibraheem482@gmail.com>
2023-01-11 14:43:33 +0530

    items style changed

--- demo.js
+++ demo.js
@@ -2,9 +2,13 @@ document.querySelector("#header-title").style.border = "2px solid black";
 document.querySelector(".title").style.color = "green";
 document.querySelector(".title").style.fontWeight = "bold";
 var items = document.getElementsByClassName("list-group-item");
-items[2].style.backgroundColor = "green";
-for (let i = 0; i < items.length; i++) {
-  items[i].style.fontWeight = "bold";
+
+var li2 = document.querySelector("#l2");
+li2.style.backgroundColor = "green";
+var li3 = document.querySelector("#l3");
+li3.style.display = "none";
+document.querySelectorAll(".list-group-item")[1].style.color = "green";
+var list = document.querySelectorAll(".list-group-item");
+for (let i = 0; i < list.length; i += 2) {
+  list[i].style.backgroundColor = "green";
 }
-var li = document.getElementsByTagName("li");
-li[4].style.color = "red";
