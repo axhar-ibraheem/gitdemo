0bd3639aecf25fc6d2e5553724c4ff6bbf50d4d4
axhar-ibraheem <azharibraheem482@gmail.com>
2023-01-11 13:40:05 +0530

    both files modified

--- demo.js
+++ demo.js
@@ -6,3 +6,5 @@ items[2].style.backgroundColor = "green";
 for (let i = 0; i < items.length; i++) {
   items[i].style.fontWeight = "bold";
 }
+var li = document.getElementsByTagName("li");
+li[4].style.color = "red";
