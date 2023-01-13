439a218debf659c4eba86d95666c4c9afdc3ae2e
axhar-ibraheem <azharibraheem482@gmail.com>
2023-01-13 23:14:19 +0530

    edit function edit

--- demo.js
+++ demo.js
@@ -16,16 +16,25 @@ function submitForm(e) {
   localStorage.setItem(`${email.value}`, userDetailsString);
   var li = document.createElement("li");
   li.className = "item";
-  var nameTextNode = document.createTextNode(`${name.value}- `);
+  var nameTextNode = document.createTextNode(`${name.value} - `);
   var emailTextNode = document.createTextNode(email.value);
-  var phoneTextNode = document.createTextNode(` -${phone.value} `);
+  var phoneTextNode = document.createTextNode(` - ${phone.value} `);
   li.append(nameTextNode, emailTextNode, phoneTextNode);
+  ///adding edit and delete buttons
   var delButton = document.createElement("button");
+  var editButton = document.createElement("button");
   delButton.style.color = "black";
-  delButton.style.background = "lightgray";
+  delButton.style.background = "lightbrown";
+  editButton.style.color = "black";
+  editButton.style.background = "lightbrown";
+  editButton.style.marginLeft = "5px";
   delButton.className = "delete";
-  delButton.appendChild(document.createTextNode("delete"));
-  li.appendChild(delButton);
+  editButton.className = "edit";
+  var editButtonNode = document.createTextNode("Edit");
+  var delButtonNode = document.createTextNode("delete");
+  delButton.appendChild(delButtonNode);
+  editButton.appendChild(editButtonNode);
+  li.append(delButton, editButton);
   list.appendChild(li);
 }
 //function to remove user from ui and local storage.
@@ -35,5 +44,20 @@ function deleteUser(e) {
     var li = e.target.parentElement;
     localStorage.removeItem(li.childNodes[1].nodeValue);
     list.removeChild(li);
+  }
+} //
+
+//edit user functionality
+list.addEventListener("click", editUser);
+function editUser(e) {
+  if (e.target.classList.contains("edit")) {
+    var li = e.target.parentElement;
+    var email = li.childNodes[1].nodeValue;
+    var obj = JSON.parse(localStorage.getItem(email));
+    document.getElementById("name").value = obj.name;
+    document.getElementById("email").value = obj.email;
+    document.getElementById("phone").value = obj.phone;
+    list.removeChild(li);
+    localStorage.removeItem(obj.email);
   }
 }
