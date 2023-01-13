9be61ea4c687ed6375e1f3716d6bbf568e95281f
axhar-ibraheem <azharibraheem482@gmail.com>
2023-01-13 15:36:28 +0530

    user details added to local storage

--- demo.js
+++ demo.js
@@ -1,14 +1,23 @@
 var form = document.getElementById("my-form");
+var list = document.getElementById("users");
 form.addEventListener("submit", submitForm);
 function submitForm(e) {
   e.preventDefault();
   var name = document.getElementById("name");
   var email = document.getElementById("email");
+  var phone = document.getElementById("phone");
   var userDetails = {
     name: name.value,
     email: email.value,
+    phone: phone.value,
   };
-  var count = 0;
   var userDetailsString = JSON.stringify(userDetails);
-  localStorage.setItem(`userDetails: ${email.value}`, userDetailsString);
+  localStorage.setItem(`${email.value}`, userDetailsString);
+  var li = document.createElement("li");
+  li.className = "item";
+  var nameTextNode = document.createTextNode(name.value);
+  var emailTextNode = document.createTextNode(` - ${email.value}`);
+  var phoneTextNode = document.createTextNode(` - ${phone.value}`);
+  li.append(nameTextNode, emailTextNode, phoneTextNode);
+  list.appendChild(li);
 }
