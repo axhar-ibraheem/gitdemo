7e661236bdab9015cb6c2fb550653694e3e59ddd
axhar-ibraheem <azharibraheem482@gmail.com>
2023-02-16 09:30:21 +0530

    crudcrud usage

--- demo.js
+++ demo.js
@@ -1,63 +1,72 @@
 var form = document.getElementById("my-form");
 var list = document.getElementById("users");
-//function to add users to ui and local storage
+ 
 form.addEventListener("submit", submitForm);
+
 function submitForm(e) {
+
   e.preventDefault();
-  var name = document.getElementById("name");
-  var email = document.getElementById("email");
-  var phone = document.getElementById("phone");
-  var userDetails = {
+  const name = document.getElementById("name");
+  const email = document.getElementById("email");
+  const phone = document.getElementById("phone");
+  const userDetails = {
     name: name.value,
     email: email.value,
     phone: phone.value,
   };
-  var userDetailsString = JSON.stringify(userDetails);
-  localStorage.setItem(`${email.value}`, userDetailsString);
-  var li = document.createElement("li");
+  
+  axios.post("https://crudcrud.com/api/fa6ed1c346604722ad845f3eab545f9d/appointmentData", userDetails).then((res)=>{
+    
+    console.log(res.data)
+  }).catch(err=>console.log(err))
+  showUserOnScreen(userDetails)
+}
+ 
+//showing the users on the UI.
+
+function showUserOnScreen(userDetails){
+
+  const li = document.createElement("li");
   li.className = "item";
-  var nameTextNode = document.createTextNode(`${name.value} - `);
-  var emailTextNode = document.createTextNode(email.value);
-  var phoneTextNode = document.createTextNode(` - ${phone.value} `);
+  const nameTextNode = document.createTextNode(`${userDetails.name} - `);
+  const emailTextNode = document.createTextNode(userDetails.email);
+  const phoneTextNode = document.createTextNode(` - ${userDetails.phone} `);
+
   li.append(nameTextNode, emailTextNode, phoneTextNode);
   ///adding edit and delete buttons
-  var delButton = document.createElement("button");
-  var editButton = document.createElement("button");
-  delButton.style.color = "black";
-  delButton.style.background = "lightbrown";
-  editButton.style.color = "black";
-  editButton.style.background = "lightbrown";
+  const delButton = document.createElement("button");
+  const editButton = document.createElement("button");
+  delButton.classList.add("button", "delete")
+  editButton.classList.add("button", "edit")
   editButton.style.marginLeft = "5px";
-  delButton.className = "delete";
-  editButton.className = "edit";
-  var editButtonNode = document.createTextNode("Edit");
-  var delButtonNode = document.createTextNode("delete");
+  const editButtonNode = document.createTextNode("Edit");
+  const delButtonNode = document.createTextNode("delete");
   delButton.appendChild(delButtonNode);
   editButton.appendChild(editButtonNode);
   li.append(delButton, editButton);
   list.appendChild(li);
 }
-//function to remove user from ui and local storage.
+
+// function to remove user from ui and .
 list.addEventListener("click", deleteUser);
 function deleteUser(e) {
   if (e.target.classList.contains("delete")) {
     var li = e.target.parentElement;
-    localStorage.removeItem(li.childNodes[1].nodeValue);
     list.removeChild(li);
   }
 } //
 
 //edit user functionality
-list.addEventListener("click", editUser);
-function editUser(e) {
-  if (e.target.classList.contains("edit")) {
-    var li = e.target.parentElement;
-    var email = li.childNodes[1].nodeValue;
-    var obj = JSON.parse(localStorage.getItem(email));
-    document.getElementById("name").value = obj.name;
-    document.getElementById("email").value = obj.email;
-    document.getElementById("phone").value = obj.phone;
-    list.removeChild(li);
-    localStorage.removeItem(obj.email);
-  }
-}
+// list.addEventListener("click", editUser);
+// function editUser(e) {
+//   if (e.target.classList.contains("edit")) {
+//     var li = e.target.parentElement;
+//     var email = li.childNodes[1].nodeValue;
+//     var obj = JSON.parse(localStorage.getItem(email));
+//     document.getElementById("name").value = obj.name;
+//     document.getElementById("email").value = obj.email;
+//     document.getElementById("phone").value = obj.phone;
+//     list.removeChild(li);
+//     localStorage.removeItem(obj.email);
+//   }
+// }
